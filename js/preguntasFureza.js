1dbe3d8cc3ca6bb8280475d6d78af25382f551c6
MadoScientista <samuel.cortes.n@gmail.com>
2026-08-20 15:42:30 -0400

    Creación de directorios y ficheros

---
+++ js/preguntasFureza.js
@@ -0,0 +1,14 @@
+const preguntas = [
+    {
+        enunciado: "",
+        imagen: "",
+        alternativas: [
+            1,
+            2,
+            3,
+            4
+        ],
+        correcta: 1
+    },
+    
+]
