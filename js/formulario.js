f5a3a61a1c26a9a06312e3b1f620bb96a0df12d4
MadoScientista <samuel.cortes.n@gmail.com>
2026-08-25 11:30:05 -0400

    Formulario muestra mensaje enviado

--- js/formulario.js
+++ js/formulario.js
@@ -1,13 +1,25 @@
 
 const formularioContacto = document.getElementById("formularioContacto")
-
+const formularioEnviadoContenedor = document.getElementById("formulario-enviado-contenedor")
+const nombreEnviado = document.getElementById("nombre-enviado")
+const correoEnviado = document.getElementById("correo-enviado")
+const motivoEnviado = document.getElementById("motivo-enviado")
+const mensajeEnviado = document.getElementById("mensaje-enviado")
 
 formularioContacto.addEventListener("submit", e => {
     e.preventDefault()
     
     const datos = new FormData(formularioContacto)
-    console.log(datos.get("nombreUsuario"))
-    console.log(datos.get("correoUsuario"))
-    console.log(datos.get("motivoUsuario"))
-    console.log(datos.get("mensajeUsuario"))
+    const datosJson = Object.fromEntries(datos.entries())
+
+    formularioEnviadoContenedor.classList.remove("d-none")
+    
+    nombreEnviado.textContent = datos.get("nombreUsuario")
+    correoEnviado.textContent = datos.get("correoUsuario")
+    motivoEnviado.textContent = datos.get("motivoUsuario")
+    mensajeEnviado.textContent = datos.get("mensajeUsuario")
+
+    formularioEnviadoContenedor.scrollIntoView()
 })
+
+formularioEnviadoContenedor.classList.add("d-none")
